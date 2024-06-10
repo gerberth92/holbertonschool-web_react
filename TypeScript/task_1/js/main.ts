97c8ca5b7e73d867306cfebe185cbf8e7ed0a515
gerberth92 <gerberthcontreras@gmail.com>
2024-06-10 13:54:55 -0500

    task 4

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -6,3 +6,42 @@ interface Teacher {
   location: string;
   [key: string]: any;
 }
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+function printTeacher(firstName: string, lastName:string) {
+  return `${firstName[0]}. ${lastName}`;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+interface clase {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface constructor {
+  new (firstName: string, lastName: string): clase;
+}
+
+class StudentClass implements clase {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework() {
+    return 'Currently working';
+  }
+
+  displayName() {
+    return this.firstName;
+  }
+}
