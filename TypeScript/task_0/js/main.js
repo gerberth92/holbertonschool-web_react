9933cccaf5ecb21d8818f0d97ed6e4e5be3df069
gerberth92 <gerberthcontreras@gmail.com>
2024-06-10 11:49:34 -0500

    update

--- TypeScript/task_0/js/main.js
+++ TypeScript/task_0/js/main.js
@@ -11,8 +11,18 @@ var student2 = {
     location: '',
 };
 var studentsList = [student1, student2];
+var table = document.createElement('table');
+var tbody = document.createElement('tbody');
 for (var _i = 0, studentsList_1 = studentsList; _i < studentsList_1.length; _i++) {
-    var estudiante = studentsList_1[_i];
-    var li = document.createElement('li');
-    li.textContent = estudiante.firstName + estudiante.location;
+    var e = studentsList_1[_i];
+    var row = document.createElement('tr');
+    var celda1 = document.createElement('td');
+    celda1.textContent = e.firstName;
+    var celda2 = document.createElement('td');
+    celda2.textContent = e.location;
+    row.appendChild(celda1);
+    row.appendChild(celda2);
+    tbody.appendChild(row);
 }
+table.appendChild(tbody);
+document.body.appendChild(table);
