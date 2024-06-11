f33bd270d98cc27f3c3e89118b9ef3dfc57d51d3
gerberth92 <gerberthcontreras@gmail.com>
2024-06-11 10:45:47 -0500

    add type

--- TypeScript/task_0/js/main.ts
+++ TypeScript/task_0/js/main.ts
@@ -21,14 +21,14 @@ const student2: Student = {
 
 const studentsList: Student[] = [student1, student2];
 
-const table = document.createElement('table');
-const tbody = document.createElement('tbody');
+const table: HTMLElement = document.createElement('table');
+const tbody: HTMLElement = document.createElement('tbody');
 
 for (const e of studentsList) {
-  const row = document.createElement('tr');
-  const celda1 = document.createElement('td');
+  const row: HTMLElement = document.createElement('tr');
+  const celda1: HTMLElement = document.createElement('td');
   celda1.textContent = e.firstName;
-  const celda2 = document.createElement('td');
+  const celda2: HTMLElement = document.createElement('td');
   celda2.textContent = e.location;
   row.appendChild(celda1);
   row.appendChild(celda2);
