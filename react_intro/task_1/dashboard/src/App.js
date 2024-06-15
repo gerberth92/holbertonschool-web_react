652f04fb593de6e0462699ee6152e9a785e5b499
gerberth92 <gerberthcontreras@gmail.com>
2024-06-15 18:15:12 -0500

    task 1

---
+++ react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,30 @@
+import logo from '../src/holberton-logo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          School dashboard
+        </p>
+      </header>
+
+      <main className='App-body'>
+        <p>
+          Login to access the full dashboard
+        </p>
+      </main>
+
+      <footer className='App-footer'>
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy(true)}
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
